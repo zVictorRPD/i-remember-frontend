45fa11b37a9aaba4d00c4d1b9c3d17091ebbedcb
Victor Martins <victor2007azevedo@hotmail.com>
2025-09-11 23:24:44 -0300

    refactor: Removendo categorias desnecessárias

--- src/interfaces/category.ts
+++ src/interfaces/category.ts
@@ -8,9 +8,6 @@ export const categoryOptions: ICategory[] = [
   { value: "Casamento", label: "Casamento" },
   { value: "Formatura", label: "Formatura" },
   { value: "Festa", label: "Festa" },
-  { value: "Reunião", label: "Reunião" },
   { value: "Viagem", label: "Viagem" },
-  { value: "Trabalho", label: "Trabalho" },
-  { value: "Pessoal", label: "Pessoal" },
   { value: "Outros", label: "Outros" },
 ];
